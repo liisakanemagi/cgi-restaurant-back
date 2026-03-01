c4e24e9a53cbef560827aaf3804e35efa7a43184
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-01 15:38:36 +0200

    feat: generate and refine JPA entities for RestaurantTable and Reservation

---
+++ src/main/java/cgi/test/restaurant_reservation/persistence/Reservation.java
@@ -0,0 +1,44 @@
+package cgi.test.restaurant_reservation.persistence;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "table_id", nullable = false)
+    private RestaurantTable restaurantTable;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "customer_name", nullable = false, length = 100)
+    private String customerName;
+
+    @NotNull
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @NotNull
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @NotNull
+    @Column(name = "guest_count", nullable = false)
+    private Integer guestCount;
+
+}
---
+++ src/main/java/cgi/test/restaurant_reservation/persistence/RestaurantTable.java
@@ -0,0 +1,49 @@
+package cgi.test.restaurant_reservation.persistence;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "restaurant_table")
+public class RestaurantTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 10)
+    @NotNull
+    @Column(name = "table_number", nullable = false, length = 10)
+    private String tableNumber;
+
+    @NotNull
+    @Column(name = "capacity", nullable = false)
+    private Integer capacity;
+
+    @NotNull
+    @Column(name = "coordinate_x", nullable = false)
+    private Double coordinateX;
+
+    @NotNull
+    @Column(name = "coordinate_y", nullable = false)
+    private Double coordinateY;
+
+    @ColumnDefault("false")
+    @Column(name = "is_window_seat")
+    private Boolean isWindowSeat = false;
+
+    @ColumnDefault("false")
+    @Column(name = "is_accessible")
+    private Boolean isAccessible = false;
+
+    @ColumnDefault("false")
+    @Column(name = "is_private")
+    private Boolean isPrivate = false;
+
+}
