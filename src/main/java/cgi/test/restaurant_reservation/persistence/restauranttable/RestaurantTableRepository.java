f9d0f173cba7da358789ecef7ff87223570c4ae6
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-03 13:13:01 +0200

    feat: Implement getValidTable functionality in service & repository

--- src/main/java/cgi/test/restaurant_reservation/persistence/restauranttable/RestaurantTableRepository.java
+++ src/main/java/cgi/test/restaurant_reservation/persistence/restauranttable/RestaurantTableRepository.java
@@ -2,5 +2,10 @@ package cgi.test.restaurant_reservation.persistence.restauranttable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Integer> {
+
+    @Override
+    Optional<RestaurantTable> findById(Integer id);
 }
