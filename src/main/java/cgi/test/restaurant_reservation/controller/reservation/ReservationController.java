1eed826ec510212fb626c086090b683829552e6c
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-04 13:43:56 +0200

    docs: add Swagger documentation for getReservations & updateRestaurantTable

--- src/main/java/cgi/test/restaurant_reservation/controller/reservation/ReservationController.java
+++ src/main/java/cgi/test/restaurant_reservation/controller/reservation/ReservationController.java
@@ -1,6 +1,7 @@
 package cgi.test.restaurant_reservation.controller.reservation;
 
 import cgi.test.restaurant_reservation.service.ReservationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @GetMapping("/api/reservations")
+    @Operation(summary = "Reserveeringute pärimine", description = "Tagastab nimekirja reserveeringutest")
 
     public List<ReservationDto> getReservations(){
         return reservationService.getReservations();
